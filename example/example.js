1cad4b720553f7cb89abebe3351d618fef957400
Paulo McNally <paulomcnally@gmail.com>
2016-10-21 23:14:21 -0600

    v0.0.2

--- example/example.js
+++ example/example.js
@@ -1,10 +1,14 @@
 var Facebook = require('../lib/fb-id');
 var facebook = new Facebook();
 
-facebook.getId('https://www.facebook.com/mcnallydev?_rdr=p', function(id) {
+facebook.getId('https://www.facebook.com/mcnallydev', function(id) {
   console.log('Page: %d', id);
 });
 
-facebook.getId('https://www.facebook.com/groups/iosdev.ni/?fref=ts', function(id) {
+facebook.getId('https://www.facebook.com/groups/iosdev.ni', function(id) {
   console.log('Group: %d', id);
 });
+
+facebook.getId('https://www.facebook.com/zr', function(id) {
+  console.log('User: %d', id);
+})
